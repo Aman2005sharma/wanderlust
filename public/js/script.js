c9c11abc068ab5f4796064d1d23e1ddca734951f
Aman Sharma <amansharma09137@gmail.com>
2026-08-09 15:20:18 +0530

    Authentication updated

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,29 @@
     }, false)
   })
 })()
+
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+if (togglePassword && password) {
+
+    togglePassword.addEventListener("click", () => {
+
+        if (password.type === "password") {
+
+            password.type = "text";
+
+            togglePassword.innerHTML =
+                '<i class="fa-regular fa-eye-slash"></i>';
+
+        } else {
+
+            password.type = "password";
+
+            togglePassword.innerHTML =
+                '<i class="fa-regular fa-eye"></i>';
+        }
+
+    });
+
+}
